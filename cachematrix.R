c0ce6326ca50bbb8154bfbf957ecb3ea3a55072d
YeshwantBhat <yeshwant.b@gmail.com>
2014-06-21 23:24:34 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,15 +7,15 @@
 ## The inverse of the matrix is cached and returned if the matrix has not changed.
 
 makeCacheMatrix <- function(x = matrix()) {
-      im <- NULL
+      im <- NULL                                  #The inverse matrix is set to NULL.
       set <- function(y) {
-        x <<- y
-        im <<- NULL
+        x <<- y                                   #Matrix is assigned to x.
+        im <<- NULL                               #Inverse of the matrix is set to NULL.
   }
-  get <- function() x
-  setinverse <- function(inverse) im <<- inverse
-  getinverse <- function() im
-  list(set = set, get = get,
+  get <- function() x                             #Returns the matrix.
+  setinverse <- function(inverse) im <<- inverse  #Caches the inverse of the matrix.
+  getinverse <- function() im                     #Returns the cached inverse of the matrix.
+  list(set = set, get = get,                      #The list is set to the various functions and returned.
        setinverse = setinverse,
        getinverse = getinverse)
 }
@@ -28,18 +28,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        if(length(x) == 0 || length(x$get()) == 0) {
+        if(length(x) == 0 || length(x$get()) == 0) {   #Returns the null object for emply list or empty matrix.
           message("Either empty list or empty matrix")
-          r<-NA
+          r<-NULL
           return(r)
   }
-  im<-x$getinverse()
-  if(!is.null(im)) {
+  im<-x$getinverse()                                   #Gets cached inverse of the matrix.
+  if(!is.null(im)) {                                   #Return the cached inverse matrix if available.
     message("getting cached data")
     return(im)
   }
-  data <-x$get()
-  im<-solve(data,...)
-  x$setinverse(im)
-  im
+  data <-x$get()                                       #Gets the matrix
+  im<-solve(data,...)                                  #Computes the inverse of the matrix.
+  x$setinverse(im)                                     #Caches the inverse of the matrix
+  im                                                   #Returns the inverse of the matrix.
 }
